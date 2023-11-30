23eed392dd2bb01c11d982ecbb47662e2206dded
Sohail Ali Khan <microsohail@gmail.com>
2023-11-30 18:41:31 +0530

    add controller and routes

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 8000
+const port = 8001
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
+//use express router
+app.use('/',require('./routes'));
+
+
+app.listen(port, (err) => {
+    if(err){
+        console.log(`Error in running server,${err}`);
+    }
   console.log(`Example app listening on port ${port}`)
-})
+});
